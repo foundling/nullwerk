181d009acbfac464baf5d4af33595bade19a3f15
Alex Ramsdell <alexramsdell@gmail.com>
2017-04-03 20:18:52 -0700

    minor cleanup

--- src/services/LocalStorage.js
+++ src/services/LocalStorage.js
@@ -1,4 +1,4 @@
-const STORAGE_KEY = 'ei-nullwerk';
+const STORAGE_KEY = 'nullwerk';
 const localStorage = localStorage || window.localStorage;
 
 /*
@@ -10,38 +10,55 @@ const localStorage = localStorage || window.localStorage;
  *
  */
 
+const errorMsgs = {
+    notSupported: "[ Error ] LocalStorage is not supported in your browser. Your settings won't be saved."
+};
+
 class Store {
 
     constructor({ defaults }) {
-        if (!localStorage) return console.log("[ Error ] LocalStorage is not supported in your browser. Your settings won't be saved.");
-        const initialized = !!localStorage.getItem(STORAGE_KEY);
-        if (!initialized) localStorage.setItem(STORAGE_KEY, JSON.stringify(defaults));
-        this.config = JSON.parse( localStorage.getItem(STORAGE_KEY) );
-        this.config.currentPreset = initialized ? this.config.presets[ this.config.currentPresetName ] : this.config.presets.default; 
-    }
 
+        if (!localStorage) {
+            return console.log(errorMsgs.notSupported);
+        }
+
+        const initialized = !!localStorage.getItem(STORAGE_KEY);
+
+        if (!initialized) {
+            let defaultStore = JSON.stringify(defaults); 
+            localStorage.setItem(STORAGE_KEY, defaultStore);
+        }
+
+        this.config = JSON.parse( localStorage.getItem(STORAGE_KEY) );
+        this.config.currentPreset = initialized ? 
+                                    this.config.presets[ this.config.currentPresetName ] : 
+                                    this.config.presets.default; 
+    }
     _saveToLocalStorage() {
     }
-
     updatePreset(name, settings) {
-        this.addPreset(name, settings);
-    }
 
+        this.addPreset(name, settings);
+
+    }
     addPreset(name, settings) {
 
         this.config.presets[ name ] = settings;
-        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.config, null, 2));
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.config));
         const updatedConfig = JSON.parse(localStorage.getItem(STORAGE_KEY));
         this.config = updatedConfig;
 
     }
     getConfig() {
+
         return this.config;
+
     }
     get allPresets() {
-        return this.config.presets;
-    }
 
+        return this.config.presets;
+
+    }
 
 };
 
